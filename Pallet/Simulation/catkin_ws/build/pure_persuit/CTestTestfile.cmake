51ea1550edd4fcb46b6b82c28a71a33b4a06a60b
mls <631945802@qq.com>
2021-12-27 17:20:51 +0800

    nothing

--- Pallet/Simulation/catkin_ws/build/pure_persuit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mls/Documents/Git/Planning/planning/Pallet/Simulation/catkin_ws/src/pure_persuit
-# Build directory: /home/mls/Documents/Git/Planning/planning/Pallet/Simulation/catkin_ws/build/pure_persuit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Pallet/Simulation/catkin_ws/build/waypoint_updater/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mls/Documents/Git/Planning/planning/Pallet/Simulation/catkin_ws/src/waypoint_updater
-# Build directory: /home/mls/Documents/Git/Planning/planning/Pallet/Simulation/catkin_ws/build/waypoint_updater
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
